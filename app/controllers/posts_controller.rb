0a437e8b0c37fc0064349915698d1136712fe2e3
kpietkaa <kpietkaa@gmail.com>
2016-10-12 15:34:23 +0200

    DRY posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.order('created_at DESC')
   end
@@ -19,17 +21,13 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    if @post.update(params[:post].permit(:title, :body))
+    if @post.update(post_params)
       redirect_to @post
     else
       render 'edit'
@@ -37,14 +35,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to root_path
   end
 
   private
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post ||= Post.find(params[:id])
   end
 end
